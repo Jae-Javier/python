774476b9056d0c4156a425a279e1f6ba0f944f69
Jae-Javier <100955294+Jae-Javier@users.noreply.github.com>
2022-04-27 19:52:07 -0400

    Hello World

---
+++ ML/test.py
@@ -0,0 +1,26 @@
+print(1 + 3)
+print(4 > 3)  # True
+
+x = 3
+y = 4
+
+t = True
+f = False
+
+print(t or f)  # prints True
+print(t and f)  # prints False
+print(not f and t)  # prints True
+
+
+print(((x < 5) and (y > 3) and (x + y < y)))
+# or
+# and
+# == is equal to
+# != is not equal to
+x = 1
+if (x > 2):
+    print('ur mom gay')
+elif (x > 3212):
+    print('idk')
+else:
+    print('ur mom not gay')
